668264f61a71d0a250ee017a46f159f200f23414
woseibonsu <40476387+woseibonsu@users.noreply.github.com>
2018-09-10 11:28:55 -0400

    Finished

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
-
+import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.print("What is your first name? ");
+        Scanner kbInput = new Scanner(System.in);
+        String fName = kbInput.next();
+        System.out.print("What is your last name? ");
+        kbInput = new Scanner(System.in);
+        String lName = kbInput.next();
+        System.out.println("Your full name is " + fName + " " + lName + ".");
+
+
     }
 }
